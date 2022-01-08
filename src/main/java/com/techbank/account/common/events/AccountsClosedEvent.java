5d2cb921766d58e8c3f51fe7da93f16bc4e7e441
PavelBlinets <pavelblinets@gmail.com>
2022-01-08 23:17:48 +0300

    command controller

--- src/main/java/com/techbank/account/common/events/AccountsClosedEvent.java
+++ src/main/java/com/techbank/account/common/events/AccountsClosedEvent.java
@@ -2,9 +2,11 @@ package com.techbank.account.common.events;
 
 import com.techbank.cqrs.core.events.BaseEvent;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@NoArgsConstructor
 @SuperBuilder
 public class AccountsClosedEvent extends BaseEvent {
 }

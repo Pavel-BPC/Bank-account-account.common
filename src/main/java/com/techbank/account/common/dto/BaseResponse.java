990f8595588767aced30771fa35aacc1bca5a8a7
PavelBlinets <pavelblinets@gmail.com>
2022-02-09 21:31:06 +0300

    Query controller

--- src/main/java/com/techbank/account/common/dto/BaseResponse.java
+++ src/main/java/com/techbank/account/common/dto/BaseResponse.java
@@ -3,8 +3,10 @@ package com.techbank.account.common.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BaseResponse {
